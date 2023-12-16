96f32783b9db7a04f25f017ec114efd078f5cae4
MOTOKI KYAN <54702789+candypopbeat@users.noreply.github.com>
2023-12-16 12:16:08 +0900

    Update duplicator.cmd

--- duplicator.cmd
+++ duplicator.cmd
@@ -1,3 +1,3 @@
-docker cp ./duplicator/. wordpress-docker-template-WordPress-1:/var/www/html/
-docker exec wordpress-docker-template-WordPress-1 chown -R www-data:www-data ./
+docker cp ./duplicator/. wordpress-docker-light-WordPress-1:/var/www/html/
+docker exec wordpress-docker-light-WordPress-1 chown -R www-data:www-data ./
 start http://localhost:8090/installer.php
